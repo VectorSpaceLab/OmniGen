fix: prevent float16 numerical overflow
Adds a small utility to scheduler to find the minimum clip bound to prevent NaNs from popping out of the decoder layers. Search over hardcoded buffer to discard as little information as possible.

Phi3Transformer now raises OverflowError when NaNs encountered.

Initialize model dtype based on actual weight value to avoid bad casts when quantized.

OmniGen/model.py
@@ -201,7 +201,7 @@ class OmniGen(nn.Module, PeftAdapterMixin):
         self.dtype = None
     
     @classmethod
-    def from_pretrained(cls, model_name: str|os.PathLike, dtype: torch.dtype = torch.bfloat16, quantization_config: BitsAndBytesConfig = None, low_cpu_mem_usage: bool = True,):
+    def from_pretrained(cls, model_name: str|os.PathLike, dtype: torch.dtype = None, quantization_config: BitsAndBytesConfig = None, low_cpu_mem_usage: bool = True,):
         model_path = Path(model_name)
         config_loc = model_name # these only diverge when model_name is *.safetensors or *.pt file
 
@@ -228,6 +228,9 @@ class OmniGen(nn.Module, PeftAdapterMixin):
                 torch.load(model_path, map_location='cpu'))
         
         config = Phi3Config.from_pretrained(config_loc)
+        # avoid inadvertently leaving the weights as float32
+        if dtype is None:
+            dtype = config.torch_dtype
 
         if hasattr(config, 'quantization_config'):
             if quantization_config is not None:
@@ -257,7 +260,9 @@ class OmniGen(nn.Module, PeftAdapterMixin):
             model = cls(config)
             model.load_state_dict(ckpt)
         
-        model.dtype = dtype
+        
+        # determine dtype via x_emb bias since as a Conv2d bias, it should never be quantized
+        model.dtype = model.x_embedder.proj.bias.dtype 
         
         del ckpt
         torch.cuda.empty_cache()

OmniGen/scheduler.py
@@ -1,10 +1,13 @@
+import copy
 from tqdm import tqdm
 from typing import Optional, Dict, Any, Tuple, List
 import gc
 
 import torch
 from transformers.cache_utils import Cache, DynamicCache, OffloadedCache
+from diffusers.utils import logging
 
+logger = logging.get_logger(__name__) 
 
 
 class OmniGenCache(DynamicCache):
@@ -121,8 +124,30 @@ class OmniGenScheduler:
         t = torch.linspace(0, 1, num_steps+1)
         t = t / (t + time_shifting_factor - time_shifting_factor * t)
         self.sigma = t
-
     
+    @torch.no_grad()
+    def _fp16_clip_autoset(self, model_llm, z, func, model_kwargs):
+        '''Recursively search for a minimal clipping value for fp16 stability'''
+        timesteps = torch.full(size=(len(z), ), fill_value=self.sigma[0], device=z.device)
+        _nan_expon = model_kwargs.pop('_nan_expon', None)
+        if _nan_expon is not None:
+            clip_val = 2**16 - 2**_nan_expon # fp16 overflows after ±2^16-32
+            model_llm.set_clip_val(clip_val)
+            
+        try:
+            _model_kwargs = copy.deepcopy(model_kwargs)
+            _model_kwargs['use_kv_cache']=False # no cache while searching
+            _, _ = func(z.clone(), timesteps, past_key_values=None, **_model_kwargs)
+        except OverflowError:
+            if _nan_expon is None:
+                logger.info('FP16 overflow, searching for clamp bounds...')
+                _nan_expon = 5 # start at 2**5
+            
+            if _nan_expon < 15: # stop at 2**15 
+                model_kwargs['_nan_expon'] = _nan_expon+1
+                return self._fp16_clip_autoset(model_llm, z, func, model_kwargs)
+            raise OverflowError('Numerical overflow, unable to find suitable clipping bounds.')
+            
     def crop_kv_cache(self, past_key_values, num_tokens_for_img):
         # return 
         crop_past_key_values = ()

OmniGen/transformer.py
@@ -29,6 +29,11 @@ class Phi3Transformer(Phi3Model):
     Args:
         config: Phi3Config
     """
+    _clip_val: float = None  # fp16: ~ (2**16 - 2**7)
+
+    def set_clip_val(self, clip_val:float=None):
+        self._clip_val = abs(clip_val)
+
     def prefetch_layer(self, layer_idx: int, device: torch.device):
         "Starts prefetching the next layer cache"
         with torch.cuda.stream(self.prefetch_stream):
@@ -137,6 +142,8 @@ class Phi3Transformer(Phi3Model):
         for decoder_layer in self.layers:
             layer_idx += 1
 
+            if self._clip_val is not None:
+                hidden_states.clamp_(-self._clip_val, self._clip_val)
             if output_hidden_states:
                 all_hidden_states += (hidden_states,)
 
@@ -173,7 +180,8 @@ class Phi3Transformer(Phi3Model):
                 all_self_attns += (layer_outputs[1],)
 
         hidden_states = self.norm(hidden_states)
-
+        if hidden_states.isnan().any():
+            raise OverflowError('Numerical Overflow: hidden states NaNs')
         # add hidden states from the last decoder layer
         if output_hidden_states:
             all_hidden_states += (hidden_states,)